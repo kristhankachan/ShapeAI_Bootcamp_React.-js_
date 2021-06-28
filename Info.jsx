d4dfad55f1cb8644cde3dae803613e9ed0d0cd15
kristhankachan <86601212+kristhankachan@users.noreply.github.com>
2021-06-28 16:11:40 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info(){
+  return (
+      <div class="note">
+        <h1>Javascript and React.js</h1>
+        <p>The Bootcamp was Helpfull </p>
+      </div>
+  );
+
+}
+
+export default Info;
